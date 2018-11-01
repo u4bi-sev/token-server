ccc205bec3e31bb3fafaa9f7bf49666336a78077
myungjaeyu <rootcode10@gmail.com>
2018-11-01 13:49:33 +0900

    Added more routes.

--- src/server/index.js
+++ src/server/index.js
@@ -5,5 +5,7 @@ const server = express()
 server
     .get('/', (req, res) => 
         res.json({ message : 'test' }))
+    .get('/a', (req, res) => 
+        res.json({ message : 'a' }))
 
 export default server
