5a0cd1189587a73e45e93398299b500ea2a87591
myungjaeyu <rootcode10@gmail.com>
2018-11-01 15:53:30 +0900

    Added supertest.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,16 @@
 import test from 'ava'
+import request from 'supertest'
+
+import app from '../src/server'
 
 test('test', t => t.is(1, 1))
+
+test('GET /a', async t => {
+
+    const res = await request(app)
+                        .get('/a')
+
+    t.is(res.status, 200)
+    t.is(res.body.message, 'a')
+
+})
